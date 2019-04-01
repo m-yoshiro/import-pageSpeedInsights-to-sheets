69b34346bfb84f879ca3e148e55042eed6b86e69
m-yoshiro <massugu.massugu@gmail.com>
2019-04-02 08:21:27 +0900

    Get data from pagespeed api

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,42 @@
-const greeter = (person: string) => {
-  return `Hello, ${person}!`;
+import { google } from 'googleapis';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const pageSpeedApi = google.pagespeedonline('v5');
+
+const options = {
+  url: process.env.URL,
+  key: process.env.APIKEY,
+  locale: 'ja',
+  strategy: 'mobile',
+};
+
+// node.js
+const request = async (options) => {
+  const { data } = await pageSpeedApi.pagespeedapi.runpagespeed(options);
+
+  const result: object = {
+    strategy: data.lighthouseResult.configSettings.emulatedFormFactor,
+    FIRST_CONTENTFUL_PAINT_MS: {
+      id: 'FCP',
+      title: 'コンテンツの初回ペイント（FCP）',
+      ...data.loadingExperience.metrics.FIRST_CONTENTFUL_PAINT_MS,
+    },
+    FIRST_INPUT_DELAY_MS: {
+      id: 'FID',
+      title: '初回入力遅延（FID）',
+      ...data.loadingExperience.metrics.FIRST_INPUT_DELAY_MS,
+    },
+    labData: {
+      id: 'labData',
+      title: 'ラボデータ',
+      ...data.lighthouseResult.audits,
+    },
+    categories: {
+      ...data.lighthouseResult.categories,
+    }
+  }
+  console.log(result)
 }
 
-console.log(greeter)
+request(options);
