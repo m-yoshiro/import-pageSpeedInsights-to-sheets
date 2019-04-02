bee83013b137afa85ff1ffead27fbc7718f92087
m-yoshiro <massugu.massugu@gmail.com>
2019-04-03 08:11:31 +0900

    Divide by functions

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import { google } from 'googleapis';
 import * as dotenv from 'dotenv';
-dotenv.config();
+import pageSpeedApi from "./pageSpeedApi";
 
-const pageSpeedApi = google.pagespeedonline('v5');
+dotenv.config();
 
 const options = {
   url: process.env.URL,
@@ -11,32 +10,5 @@ const options = {
   strategy: 'mobile',
 };
 
-// node.js
-const request = async (options) => {
-  const { data } = await pageSpeedApi.pagespeedapi.runpagespeed(options);
-
-  const result: object = {
-    strategy: data.lighthouseResult.configSettings.emulatedFormFactor,
-    FIRST_CONTENTFUL_PAINT_MS: {
-      id: 'FCP',
-      title: 'コンテンツの初回ペイント（FCP）',
-      ...data.loadingExperience.metrics.FIRST_CONTENTFUL_PAINT_MS,
-    },
-    FIRST_INPUT_DELAY_MS: {
-      id: 'FID',
-      title: '初回入力遅延（FID）',
-      ...data.loadingExperience.metrics.FIRST_INPUT_DELAY_MS,
-    },
-    labData: {
-      id: 'labData',
-      title: 'ラボデータ',
-      ...data.lighthouseResult.audits,
-    },
-    categories: {
-      ...data.lighthouseResult.categories,
-    }
-  }
-  console.log(result)
-}
-
-request(options);
+const request = pageSpeedApi(options);
+console.log(request)
---
+++ src/pageSpeedApi.ts
@@ -0,0 +1,33 @@
+import { google } from 'googleapis';
+
+const pageSpeedApi = google.pagespeedonline('v5');
+
+export default options => {
+  return async () => {
+    const { data } = await pageSpeedApi.pagespeedapi.runpagespeed(options);
+
+    const result: object = {
+      strategy: data.lighthouseResult.configSettings.emulatedFormFactor,
+      FIRST_CONTENTFUL_PAINT_MS: {
+        id: 'FCP',
+        title: 'コンテンツの初回ペイント（FCP）',
+        ...data.loadingExperience.metrics.FIRST_CONTENTFUL_PAINT_MS,
+      },
+      FIRST_INPUT_DELAY_MS: {
+        id: 'FID',
+        title: '初回入力遅延（FID）',
+        ...data.loadingExperience.metrics.FIRST_INPUT_DELAY_MS,
+      },
+      labData: {
+        id: 'labData',
+        title: 'ラボデータ',
+        ...data.lighthouseResult.audits,
+      },
+      categories: {
+        ...data.lighthouseResult.categories,
+      }
+    }
+
+    return result
+  }
+}
